c64e5b248d4118e1315cc6634ad9c4da8bc13a90
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-01-14 00:38:19 +0100

    Makefile added

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ SRCS := $(shell find $(SRC_DIRS) -name *.cpp -or -name *.c -or -name *.s)
 OBJS := $(addsuffix .o,$(basename $(SRCS)))
 
 CFLAGS = -std=c++11
-LFLAGS = `pkg-config opencv --cflags --libs`
+LFLAGS = `pkg-config opencv --cflags --libs` -pthread
 
 OBJ = $(addsuffix .o, $(basename $(SRC)))
 
--- src/video.cpp
+++ src/video.cpp
@@ -7,6 +7,20 @@
 using namespace cv;
 using namespace std;
 
+pthread_t *workers;
+pthread_mutex_t mutexLock;
+
+int ver = 1;
+int hor = 0;
+int frameIter= 0;
+int numFrames = 0;
+Mat *inFrames;
+Mat *outFrames;
+//vector<Mat> inFrames;
+//vector<Mat> *outFrames;
+
+void *reduce(void *);
+
 static void help()
 {
     cout
@@ -16,6 +30,67 @@ static void help()
             << "./video inputvideoName"                                                         << endl
             << "------------------------------------------------------------------------------" << endl
             << endl;
+}
+
+/*
+ * Entrance function of the worker thread.
+ */
+void *reduce(void *)
+{
+    int frameId;
+    cout << "Frame " << frameIter << "/" << numFrames << endl;
+    Mat frame1, frame2, frame3, frame4, frame5;
+    while(1) {
+
+        //pthread_mutex_lock(&mutexLock);
+        if(frameIter > numFrames - 1) {
+            //pthread_mutex_unlock(&mutexLock);
+            cout << "Frame-- " << frameIter << "/" << numFrames << endl;
+            break;
+        }
+        frameId = frameIter++;
+        cout << "Frame++ " << frameIter << "/" << numFrames << endl;
+
+        //pthread_mutex_unlock(&mutexLock);
+
+//
+//
+//        frame1 = inFrames[frameId];
+//
+//        // check if we are close to the end of the video and
+//        // select frames appropriately
+//        if(frameId < numFrames - 4) {
+//            frame2 = inFrames[frameId+1];
+//            frame3 = inFrames[frameId+2];
+//            frame4 = inFrames[frameId+3];
+//            frame5 = inFrames[frameId+4];
+//        }
+//        else if(frameId < numFrames - 3) {
+//            frame2 = inFrames[frameId+1];
+//            frame3 = inFrames[frameId+2];
+//            frame4 = inFrames[frameId+3];
+//            frame5 = frame4;
+//        }
+//        else if(frameId < numFrames - 2) {
+//            frame2 = inFrames[frameId+1];
+//            frame3 = inFrames[frameId+2];
+//            frame4 = frame3;
+//            frame5 = frame3;
+//        }
+//        else if(frameId < numFrames - 1) {
+//            frame2 = inFrames[frameId+1];
+//            frame3 = frame2;
+//            frame4 = frame2;
+//            frame5 = frame2;
+//        }
+//        else {
+//            frame2 = frame1;
+//            frame3 = frame1;
+//            frame4 = frame1;
+//            frame5 = frame1;
+//        }
+        // outFrames[frameId] = reduce_frame(frame1, frame2, frame3, frame4, frame5, ver, hor);
+    }
 }
 
 int main(int argc, char **argv)
@@ -29,6 +104,7 @@ int main(int argc, char **argv)
 
     const string source = argv[1];
 
+    // Read video
     VideoCapture inputVideo(source);
     if (!inputVideo.isOpened())
     {
@@ -39,8 +115,47 @@ int main(int argc, char **argv)
     Size S = Size((int) inputVideo.get(CV_CAP_PROP_FRAME_WIDTH),
                   (int) inputVideo.get(CV_CAP_PROP_FRAME_HEIGHT));
 
+    int fps = inputVideo.get(CV_CAP_PROP_FPS);
+    int numFrames = inputVideo.get(CV_CAP_PROP_FRAME_COUNT);
+
+    cout << "Input frame resolution: Width=" << S.width << "  Height=" << S.height
+         << " of nr#: " << numFrames << endl;
+    cout << "Input codec type: " << ".avi" << endl;
+
+    inFrames = new Mat[numFrames];
+    for(int i = 0; i < numFrames; ++i) {
+        inputVideo >> inFrames[i];
+    }
+    inputVideo.release();
+
+    //--------------------------------------------------------------------------------
+
+    // Process video
+    outFrames = new Mat[numFrames];
+    int numWorkers = 4;
+    cout << "Using " << numWorkers << " workers." << endl;
+
+    workers = (pthread_t *)malloc(sizeof(pthread_t)*numWorkers);
+    for(int i = 0; i < numWorkers; ++i) {
+        cout << i << endl;
+        if(pthread_create(&workers[i], NULL, reduce, NULL)) {
+            cout << "Error creating thread\n" << endl;
+            return -1;
+        }
+    }
+
+    for(int i = 0; i < numWorkers; ++i) {
+        if(pthread_join(workers[i], NULL)) {
+            cout << "Error joining thread\n" << endl;
+            return -1;
+        }
+    }
+
+    //----------------------------------------------------------------------------------
+
+    // Write video
     VideoWriter outputVideo;
-    outputVideo.open("out.avi", CV_FOURCC('D', 'I', 'V', 'X'), inputVideo.get(CV_CAP_PROP_FPS), S, true);
+    outputVideo.open("out.avi", CV_FOURCC('D', 'I', 'V', 'X'), fps, S, true);
 
     if (!outputVideo.isOpened())
     {
@@ -48,31 +163,14 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    cout << "Input frame resolution: Width=" << S.width << "  Height=" << S.height
-         << " of nr#: " << inputVideo.get(CV_CAP_PROP_FRAME_COUNT) << endl;
-    cout << "Input codec type: " << ".avi" << endl;
-
-    int channel = 2;
-    Mat src, res;
-    vector<Mat> frames;
-
-    for(;;)
-    {
-        inputVideo >> src;
-        if (src.empty()) break;
-
-        frames.push_back(src);
-
-        imshow("mainWin", src);
-        waitKey(20);
-
-        //outputVideo.write(res); //save or
-        outputVideo << src;
+    for(int i = 0; i < numFrames; ++i){
+        outputVideo << inFrames[i];
+//        imshow("mainWin", inFrames[i]);
+//        waitKey(20);
     }
 
+    outputVideo.release();
     cout << "Finished writing" << endl;
 
-    inputVideo.release();
-    outputVideo.release();
     return 0;
 }
