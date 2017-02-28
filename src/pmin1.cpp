96cd484b0f57a2262644d74c792b396654c7ca32
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-02-28 13:47:23 +0100

    try for min calc in parallel

--- src/pmin1.cpp
+++ src/pmin1.cpp
@@ -5,6 +5,15 @@ using namespace cv;
 using namespace std;
 using namespace ff;
 
+struct PMinState {
+    PMinState(const ulong n, ulong step, vector<Point> points): n(n), step(step), points(points) {};
+    const ulong n;
+    ulong step;
+    vector<Point> points;
+};
+
+PMinState* st;
+
 ulong n = 11;
 int step = 2;
 vector<Point> points;
@@ -30,7 +39,7 @@ struct Scheduler: ff_node_t<long> {
 
     long *svc(long *task) {
         if (task == nullptr) {
-            for (long i = 1; i < n; i += step) {
+            for (long i = 1; i < st->n; i += step) {
                 ff_send_out(new long(i));
             }
             return GO_ON;
@@ -39,14 +48,14 @@ struct Scheduler: ff_node_t<long> {
         delete task;
 
         ++counter;
-        if (counter == 1 + (n - 1)/step) {
+        if (counter == 1 + (st->n - 1)/step) {
             counter = 0;
             step = step << 1;
-            for (long i = 1; i <= n; i += step)
-               ff_send_out(new long(i));
+            for (long i = 1; i <= st->n; i += step)
+                ff_send_out(new long(i));
         }
 
-        if (step > n) {
+        if (step > st->n) {
             return EOS;
         }
 
@@ -66,17 +75,19 @@ void pmin() {
     if (farm.run_and_wait_end()<0) error("running farm");
 };
 
-//int main() {
-//
-//    int s[11] = {8, 7, 1, 67, 6, 9, 4, 3, 88, 0, 7};
-//    for (int i = 0; i < n; i++) {
-//        Point *p = new Point(i, s[i]);
-//        points.push_back(*p);
-//    }
-//
-//    pmin();
-//
-//    std::cout << points.at(0).x << points.at(0).y << std::endl;
-//
-//    return 0;
-//}
+int main() {
+
+    int s[11] = {8, 7, 1, 67, 6, 9, 4, 3, 88, 0, 7};
+    for (int i = 0; i < n; i++) {
+        Point *p = new Point(i, s[i]);
+        points.push_back(*p);
+    }
+
+    st = new PMinState(11, 2, points);
+
+    pmin();
+
+    std::cout << points.at(0).x << points.at(0).y << std::endl;
+
+    return 0;
+}
