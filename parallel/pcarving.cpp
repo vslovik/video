cbd843e7a6cd338ad5b54343e0cc9e8ab8443e1b
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-04-01 22:29:10 +0200

    video re-targeting

--- parallel/pcarving.cpp
+++ parallel/pcarving.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <ff/parallel_for.hpp>
 #include "psobel.h"
 #include <opencv2/highgui/highgui.hpp>
 
 struct PMinState {
-	PMinState(const int n, std::vector<cv::Point> points):
+	PMinState(const int n, cv::Point *points):
 			n(n), points(points) {};
 	const int n;
 	int step = 2;
-	std::vector<cv::Point> points;
+	cv::Point *points;
 };
 
 uchar max_int = std::numeric_limits<uchar>::max();
 
-cv::Point parallel_min(uchar* s, const int N, int num_workers = 1){
-
-	std::vector<cv::Point> points;
-	for (int i = 0; i < N; i++) {
-		points.push_back(cv::Point(i, s[i]));
-	}
+cv::Point parallel_min(cv::Point *points, const int N, int num_workers = 1){
 
 	PMinState* st = new PMinState(N, points);
 	ff::ParallelFor pf(num_workers, false);
@@ -33,29 +30,31 @@ cv::Point parallel_min(uchar* s, const int N, int num_workers = 1){
 			ulong left = (ulong) i - 1;
 			ulong right = (ulong) cv::min(st->n - 1, i - 1 + st->step - 1);
 
-			if (st->points.at(left).y > st->points.at(right).y) {
-				st->points.at(left) = st->points.at(right);
+			if (st->points[left].y > st->points[right].y) {
+				st->points[left] = st->points[right];
 			} else {
-				st->points.at(right) = st->points.at(left);
+				st->points[right] = st->points[left];
 			}
 		});
 
 		st->step = st->step << 1;
 	}
 
-	return st->points.at(0);
+	return st->points[0];
 };
+
+
 
 int *find_seam(Mat &image, int num_workers = 1){
     int H = image.rows;
     int W = image.cols;
     int *seams;
-	uchar *scores;
 	uchar *row;
+	cv::Point *points;
 
-	row = (uchar *)malloc(W*sizeof(uchar));
-    seams = (int *)malloc(W*H*sizeof(int));
-    scores = (uchar *)malloc(W*sizeof(uchar));
+	points = (cv::Point *) malloc(W * sizeof(cv::Point));
+	row = (uchar *) malloc(W * sizeof(uchar));
+	seams = (int *) malloc(W * H * sizeof(int));
 
 	ff::ParallelFor pf(num_workers, false);
     for(int r = 0; r < H; r++){
@@ -72,14 +71,15 @@ int *find_seam(Mat &image, int num_workers = 1){
         });
 
         // Advance seams
-        pf.parallel_for(0L,W,[&row, r, W, H, &seams, &scores](int c) {
+        pf.parallel_for(0L,W,[&row, r, W, H, &seams, &points](int c) {
             if(r > 0) {
 	            uchar left = c > 0 ? row[c - 1] : max_int;
 	            uchar right = c < W - 1 ? row[c + 1] : max_int;
 	            uchar middle = row[c];
 	            uchar m = std::min({left, middle, right});
-                if(r == H - 1)
-	                scores[c] = m;
+                if(r == H - 1) {
+	                points[c] = cv::Point(c, m);
+                }
                 if(m == left)
                     seams[r * W + c] = c - 1;
                 else if(m == right)
@@ -91,7 +91,7 @@ int *find_seam(Mat &image, int num_workers = 1){
         });
     }
 
-	Point p = parallel_min(scores, W, num_workers);
+	Point p = parallel_min(points, W, num_workers);
 
 	int *path = new int[H];
     for(int r = 0; r < H; r++)
@@ -124,31 +124,37 @@ void coherence_function(Mat &image, int* seam, int num_workers = 1) {
     coherence(image, seam, 8);
 }
 
-//int main(int argc, char **argv)
-//{
-//	std::cout << "find seam" << std::endl;
-//	try {
-//		Mat image;
-//
-//
-//        image = imread("../data_/monteverdi_ritratto.jpg", 1);
-//
-//		std::time_t t0 = std::time(0);
-//
-//		for(int k = 0; k < 100; k++) {
-//			int *seam = find_seam(image, 4);
-//
-////		for(int r = 0; r < image.rows; r++) {
-////			std::cout << seam[r] << std::endl;
-////		}
+int main(int argc, char **argv)
+{
+	std::cout << "find seam" << std::endl;
+	try {
+		Mat image;
+
+
+        image = imread("../data_/monteverdi_ritratto.jpg", 1);
+
+		Mat gray;
+		cvtColor(image, gray, CV_BGR2GRAY);
+
+		Mat eimage;
+		energy_function(gray, eimage, 4);
+
+		std::time_t t0 = std::time(0);
+
+		for(int k = 0; k < 100; k++) {
+			int *seam = find_seam(image, 4);
+
+//		for(int r = 0; r < image.rows; r++) {
+//			std::cout << seam[r] << std::endl;
 //		}
-//
-//		std::cout <<  std::time(0) - t0 << std::endl;
-//
-//	} catch(std::string e){
-//		std::cout << e << std::endl;
-//		return -1;
-//	}
-//
-//	return 0;
-//}
+		}
+
+		std::cout <<  std::time(0) - t0 << std::endl;
+
+	} catch(std::string e){
+		std::cout << e << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
