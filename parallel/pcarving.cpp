6d80e75d4e7c0183f6aea27dc115e7fc96ef0a82
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-04-05 22:45:41 +0200

    start to search several seams at the same time

--- parallel/pcarving.cpp
+++ parallel/pcarving.cpp
@@ -8,6 +8,7 @@
 
 #define CW 1;
 #define CCW 0;
+#define THRESHOLD 5;
 
 static void help()
 {
@@ -32,16 +33,22 @@ void find_seam(Mat &image, int *path, int num_workers = 1){
 	int H = image.rows;
 	int W = image.cols;
 
-	cv::Point *points = new cv::Point[W];
+	int *traces = new int[W];
+	bool *discarded = new bool[W];
+	cv::Point *epoints = new cv::Point[W];
 	uchar *row = new uchar[W];
 	uchar *next_row = new uchar[W];
 	int *seams = new int[W * H];
+
+	for(int c = 0; c < W; c++) {
+		discarded[c] = false;
+	}
 
 	ff::ParallelFor pf(num_workers, false);
 	for(int r = 0; r < H; r++){
 
 		// Calculate row values
-		pf.parallel_for(0L, W, [&next_row, row, r, W, image](int c) {
+		pf.parallel_for(0L, W, [&next_row, row, r, W, image, &traces](int c) {
 			uchar next = image.at<uchar>(r,c);
 			if(r > 0) {
 				uchar left = c > 0 ? row[c - 1] : max_int;
@@ -49,29 +56,74 @@ void find_seam(Mat &image, int *path, int num_workers = 1){
 				next += std::min({left, row[c], right});
 			}
 			next_row[c] = next;
+			traces[c] = 0;
 		});
 
 		row = next_row;
 
 		// Advance seams
-		pf.parallel_for(0L,W,[row, r, W, H, &seams, &points](int c) {
+		pf.parallel_for(0L,W,[row, r, W, H, &seams, &epoints, &traces, &discarded](int c) {
 			if(r > 0) {
+
 				int sc = seams[(r-1)*W + c];
-				uchar left = sc > 0 ? row[sc - 1] : max_int;
-				uchar right = sc < W - 1 ? row[sc + 1] : max_int;
-				uchar middle = row[sc];
-				uchar m = std::min({left, middle, right});
-				if(r == H - 1) {
-					points[c] = cv::Point(c, m);
+				if(!discarded[c]) {
+					uchar left = sc > 0 ? row[sc - 1] : max_int;
+					uchar right = sc < W - 1 ? row[sc + 1] : max_int;
+					uchar middle = row[sc];
+					uchar m = std::min({left, middle, right});
+
+					int pos;
+					if (m == left)
+						pos = sc - 1;
+					else if (m == right)
+						pos = sc + 1;
+					else
+						pos = sc;
+
+
+					//ToDo: move to the next row loop to avoid race cond
+					if (traces[pos] == 0) {
+						traces[pos] = c;
+						epoints[c] = cv::Point(c, m);
+						seams[r * W + c] = pos;
+					} else {
+//						int  thr = THRESHOLD;
+//						if(r > H * thr / 100) {
+//							int key = 0; int val = max_int + 1;
+//							for( int k = sc - 1; k < sc + 2; k++){
+//								if(k != pos && traces[k] == 0) {
+//									if(val > row[k]) {
+//										val = row[k];
+//										key = k;
+//									}
+//								}
+//							}
+//							if(val < max_int + 1) {
+//								traces[pos] = c;
+//								epoints[c] = cv::Point(c, val);
+//								seams[r * W + c] = key;
+//							} else {
+//								discarded[c] = true;
+//								epoints[c] = cv::Point(c, max_int);
+//							}
+//						} else {
+							int index = traces[pos];
+							if (epoints[index].y > m) {
+								discarded[c] = true;
+								epoints[c] = cv::Point(c, max_int); //ToDo
+							} else {
+								discarded[index] = true;
+								epoints[index] = cv::Point(c, max_int); //ToDo: not needed
+
+								epoints[c] = cv::Point(c, m);
+								seams[r * W + c] = pos;
+							}
+//						}
+					}
 				}
-				if(m == left)
-					seams[r * W + c] = sc - 1;
-				else if(m == right)
-					seams[r * W + c] = sc + 1;
-				else
-					seams[r * W + c] = sc;
 			} else
-				seams[r*W + c] = c;
+				seams[r * W + c] = c;
+
 		});
 	}
 
@@ -81,14 +133,14 @@ void find_seam(Mat &image, int *path, int num_workers = 1){
 
 	while (true) {
 
-		pf.parallel_for(1, (long) W, (long) step, [W, step, &points](int i) {
+		pf.parallel_for(1, (long) W, (long) step, [W, step, &epoints, &discarded](int i) {
 			ulong left = (ulong) i - 1;
 			ulong right = (ulong) cv::min(W - 1, i - 1 + step - 1);
 
-			if (points[left].y > points[right].y) {
-				points[left] = points[right];
+			if (epoints[left].y > epoints[right].y) {
+				epoints[left] = epoints[right];
 			} else {
-				points[right] = points[left];
+				epoints[right] = epoints[left];
 			}
 		});
 
@@ -99,9 +151,9 @@ void find_seam(Mat &image, int *path, int num_workers = 1){
 		step = step << 1;
 	}
 
-	Point p = points[0];
+	Point p = epoints[0];
 
-	delete[] points;
+	delete[] epoints;
 
 	pf.parallel_for(0, (long)H, [W, &path, p, seams, &image](int r) {
 		path[r] = seams[r * W + p.x];
